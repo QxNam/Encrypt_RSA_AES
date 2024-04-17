f71a9cb15dcd8b5c82a0d383a3b6ae828021708e
qxnam <quachnam311@gmail.com>
2024-04-17 22:18:45 +0700

    app

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import streamlit as st
 from rsa import *
+from aes import AES
+import random, string
 
 tab1, tab2 = st.tabs(['RSA', 'AES'])
 with tab1:
@@ -31,7 +33,7 @@ with tab1:
          p_gen, q_gen = random_p_q()
          st.session_state['generated_p'] = float(p_gen)
          st.session_state['generated_q'] = float(q_gen)
-         # st.experimental_rerun()
+         st.experimental_rerun()
 
       n = p*q if (p and q) else None
       phi = (p-1)*(q-1) if n else None
@@ -71,13 +73,14 @@ with tab1:
             st.write(f"d = {d}", check_d(d, e, phi))
          else:
             st.write("⚠️ Please fill box d", d)
+         # st.experimental_rerun()
          
       if gen_e_d:
          e_gen = select_e(phi)
          d_gen = generate_d(e_gen, phi)
          st.session_state['generated_e'] = float(e_gen)
          st.session_state['generated_d'] = float(d_gen)
-         # st.experimental_rerun()
+         st.experimental_rerun()
 
    with st.form("en_de_rsa"):
       en_col, opt, de_col = st.columns([2,1,2])
@@ -95,18 +98,61 @@ with tab1:
             message_en = encrypt((n, e), message_en)
             st.session_state['encrypt_text'] = None
             st.session_state['decrypt_text'] = message_en['text']
-            # st.experimental_rerun()
+            st.experimental_rerun()
       if de_btn:
          if message_de:
             message_de = decrypt((n, d), message_de)
             st.session_state['encrypt_text'] = message_de['text']
             st.session_state['decrypt_text'] = None
-            # st.experimental_rerun()
+            st.experimental_rerun()
 
 
 with tab2:
    st.header("AES")
+   # encryption
+   with st.form("param_aes"):
+      btn_key_aes = st.text_input(label='Input key:', value=st.session_state.get('key_aes', ''), max_chars=16, key="key_encrypt_aes")
+      if len(btn_key_aes) != 16:
+         st.error('Mã khóa cần có 16 ký tự.')
+      key_encrypt = st.form_submit_button(label='Generate key')
+      if key_encrypt:
+         st.session_state['key_aes'] = ''.join(random.choice(string.ascii_letters) for _ in range(16))
+         st.experimental_rerun()
 
+   # decryption
+   with st.form("main"):
+      en_col, opt, de_col = st.columns([2,1,2])
+      with en_col:
+         message_en = st.text_area(label="Encrypt text", value=st.session_state.get('encrypt_text', None), placeholder="Input text")
+      with opt:
+         st.write("")
+         en_btn = st.form_submit_button(label="Encrypt >>", )
+         de_btn = st.form_submit_button(label="<< Decrypt", )
+      with de_col:
+         message_de = st.text_area(label="Decrypt", value=st.session_state.get('decrypt_text', None))
+
+      if en_btn:
+         if message_en!=None:
+            aes = AES(int.from_bytes(btn_key_aes.encode(), 'big'))
+
+            encrypted_text = aes.encrypt(int.from_bytes(message_en.encode(), 'big'))
+            encrypted_text_hex = hex(encrypted_text)[2:]
+            st.session_state['encrypt_text'] = None
+            st.session_state['decrypt_text'] = encrypted_text_hex
+            st.experimental_rerun()
+
+      if de_btn:
+         if message_de!=None:
+            aes = AES(int.from_bytes(btn_key_aes.encode(), 'big'))
+
+            ciphertext_bytes = bytes.fromhex(message_de)
+            ciphertext_int = int.from_bytes(ciphertext_bytes, byteorder='big')
+            decrypted_int = aes.decrypt(ciphertext_int)
+            byte_length = (decrypted_int.bit_length() + 7) // 8 or 16
+            decrypted_text = decrypted_int.to_bytes(byte_length, 'big').decode(errors='ignore').rstrip('\x00')
+            st.session_state['encrypt_text'] = decrypted_text
+            st.session_state['decrypt_text'] = None
+            st.experimental_rerun()
 
 # cho text st.header("RSA") ra giữa màn hình
 css = """
@@ -120,12 +166,12 @@ h2 {
    font-size: 23; 
    font-weight: bold; 
    color: red; 
-   text-align: center
+   text-align: center;
 }
 
 div.stButton > button:first-child {
-    margin: 5px auto;  /* Đẩy nút bấm ra giữa và thêm một chút đệm */
-    display: block;  /* Khiến cho nút bấm hiển thị dạng block */
+    margin: 5px auto;
+    display: block;
 }
 """
 
